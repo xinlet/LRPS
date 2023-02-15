609aee6290840a354fa8a3cc75dfdb5d286ac1b9
Xinle Tian <72056199+xinlet@users.noreply.github.com>
2023-02-15 10:57:16 +0000

    Add files via upload

---
+++ R/Synthetic_DFM_2.R
@@ -0,0 +1,30 @@
+#####Synthetic DFM without idio structures######
+#fixed
+n = 100 # number of observations
+N = 60 # number of variables 
+p = 1 # VAR(p) model for factors 
+s = 1 # weight of true non-zero loadings
+
+#tweaking parameters
+r = 10 # number of factors 
+rho = 0.7 # VAR(p) auto-correlation coefficient  (0.3,0.8)
+
+x <- 1
+repeat {
+  A = rho*diag(r)
+  Sig_u = diag(r) #fixed sig_u = 1
+  factors = tsDyn::VAR.sim( B = A, n = n, include = "none",  varcov = Sig_u)
+  
+  Lambda_dense = matrix(rnorm(N*r), ncol = r)
+
+  e = matrix(rnorm(n*N), ncol = N)
+ 
+  X = factors %*% t(Lambda_dense) + e
+  
+  form = sprintf('subject_%s.csv', x)
+  write.csv(X, file = form, row.names = FALSE)
+  x = x+1
+  if (x == 101) {
+    break
+  }
+}
