3e091e23196180ea49039d64e2aae083b9edb43b
Xinle Tian <72056199+xinlet@users.noreply.github.com>
2023-02-15 12:58:58 +0000

    Add files via upload

---
+++ R/choose_k.R
@@ -0,0 +1,92 @@
+#realdata=====================================
+setwd("~/UoB/Code/Data_Atlanta fMRI")
+filenames <- list.files(path = "~/UoB/Code/Data_Atlanta fMRI",  
+                        ("csv"))
+
+filenames <- filenames[1:length(filenames)]
+rollingwindow = 120
+#====
+
+analyzek <- function(filename) {
+  y <- read.csv(file = filename, header = TRUE)
+  y <- data.matrix(y)
+  results = array(0, dim = c(116, (nrow(y) - rollingwindow - 1))) # where to store the results
+  for(i in 1:(nrow(y) - rollingwindow - 1)) {
+    y_train <- y[i: (i-1+rollingwindow),]   #training data
+    #y_test <- y[(i+ rollingwindow),]
+    
+    pc <- prcomp(y_train,
+                 center = TRUE,
+                 scale. = TRUE)
+    choose_k <- cumsum(pc$sdev^2 / sum(pc$sdev^2))
+    results[,i] <- c(choose_k)
+  }
+  Meanresultsind <- c(rowMeans(results))
+  return(Meanresultsind)
+}
+
+result_k  <- matrix(NA, 116, length(filenames))
+
+for (i in 1:length(filenames)) {
+  filenamesuse <- filenames[i]
+  output <- analyzek(filenamesuse) 
+  result_k[,i] = output
+}
+
+k_mean <- rowMeans(result_k)
+k_num <- c(1:116)
+matrix_k <- as.data.frame(cbind(k_num, k_mean))
+
+ggplot(matrix_k) + geom_line(aes(x = k_num, y = k_mean)) +
+  labs(y= "Cumulative explained Variance", x = "Number of k") +
+  scale_x_continuous(breaks = seq(0, 116, by = 10)) +
+  geom_hline(yintercept=0.9, linetype="dashed", color = "red") + 
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1)) 
+
+
+#Pilot version of choosing k====
+#using a subset 5 subjects
+setwd("~/UoB/Code/Data_Atlanta fMRI")
+filenames <- list.files(path = "~/UoB/Code/Data_Atlanta fMRI",  
+                        ("csv"))
+
+filenames <- filenames[1:length(filenames)][1:5]
+rollingwindow = 120
+
+analyzek <- function(filename) {
+  y <- read.csv(file = filename, header = TRUE)
+  y <- data.matrix(y)
+  results = array(0, dim = c(116, (nrow(y) - rollingwindow - 1))) # where to store the results
+  for(i in 1:(nrow(y) - rollingwindow - 1)) {
+    y_train <- y[i: (i-1+rollingwindow),]   #training data
+    #y_test <- y[(i+ rollingwindow),]
+    
+    pc <- prcomp(y_train,
+                 center = TRUE,
+                 scale. = TRUE)
+    choose_k <- cumsum(pc$sdev^2 / sum(pc$sdev^2))
+    results[,i] <- c(choose_k)
+  }
+  Meanresultsind <- c(rowMeans(results))
+  return(Meanresultsind)
+}
+
+result_k  <- matrix(NA, 116, length(filenames))
+
+for (i in 1:length(filenames)) {
+  filenamesuse <- filenames[i]
+  output <- analyzek(filenamesuse) 
+  result_k[,i] = output
+}
+
+k_mean <- rowMeans(result_k)
+k_num <- c(1:116)
+matrix_k <- as.data.frame(cbind(k_num, k_mean))
+
+ggplot(matrix_k) + geom_line(aes(x = k_num, y = k_mean)) +
+  labs(y= "Cumulative explained Variance", x = "Number of k") +
+  scale_x_continuous(breaks = seq(0, 116, by = 10)) +
+  geom_hline(yintercept=0.9, linetype="dashed", color = "red") + 
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1))
+
+
