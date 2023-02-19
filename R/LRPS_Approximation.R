287a981670a49fa411ab13dafd2bdeb09fb80ee3
xinlet <72056199+xinlet@users.noreply.github.com>
2023-02-19 12:49:42 +0000

    Update LRPS_Approximation.R

--- R/LRPS_Approximation.R
+++ R/LRPS_Approximation.R
@@ -19,6 +19,5 @@ LRPS_Approx <- function(y_train) {
                scale. = TRUE)
   y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
   y_train <- as.data.frame(y_train) 
-  
   sparsevar <- VARshrink(y_train, p=1, type = "none", method = "ridge")
 }
