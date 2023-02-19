0bcdf1a7784293ad03fcf9a40245fd0283fd1694
xinlet <72056199+xinlet@users.noreply.github.com>
2023-02-19 12:49:14 +0000

    Update LRPS_Approximation.R

--- R/LRPS_Approximation.R
+++ R/LRPS_Approximation.R
@@ -2,6 +2,17 @@ library(Metrics)
 library(matrixStats)
 library(VARshrink)
 #======================================
+# Low-Rank Pre-smoothed Data
+LRPS_datagenerate <- function(y_train) {
+  pc <- prcomp(y_train,
+               center = TRUE,
+               scale. = TRUE)
+  y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
+  y_train <- as.data.frame(y_train) 
+}
+
+
+# LRPS Approximation
 LRPS_Approx <- function(y_train) {
   pc <- prcomp(y_train,
                center = TRUE,
