8c6a0dc8b084b9711bce62423b590986f468ce88
xinlet <72056199+xinlet@users.noreply.github.com>
2023-02-19 12:35:42 +0000

    Update and rename Generating_newdata.R to LRPS_Approximation.R

--- R/LRPS_Approximation.R
+++ R/LRPS_Approximation.R
@@ -1,7 +1,13 @@
+library(Metrics)
+library(matrixStats)
+library(VARshrink)
+#======================================
 data_generating <- function(y_train) {
   pc <- prcomp(y_train,
                center = TRUE,
                scale. = TRUE)
   y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
   y_train <- as.data.frame(y_train) 
+  
+  sparsevar <- VARshrink(y_train, p=1, type = "none", method = "ridge")
 }
