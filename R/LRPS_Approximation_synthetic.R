0fd83fb89b03c8dfcdb0f4de9e47bc9132146a98
xinlet <72056199+xinlet@users.noreply.github.com>
2023-02-22 20:07:55 +0000

    Create LRPS_Approximation_synthetic.R

---
+++ R/LRPS_Approximation_synthetic.R
@@ -0,0 +1,25 @@
+library(Metrics)
+library(matrixStats)
+library(VARshrink)
+#======================================
+# Two functions are situations in the synthetic data testing situations
+# Where the K values are pre-set.
+#======================================
+# Low-Rank Pre-smoothed Data
+LRPS_datagenerate_synthetic <- function(y_train) {
+  pc <- prcomp(y_train,
+               center = TRUE,
+               scale. = TRUE)
+  y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
+  y_train <- as.data.frame(y_train) 
+}
+
+# LRPS Approximation
+LRPS_Approx_synthetic <- function(y_train) {
+  pc <- prcomp(y_train,
+               center = TRUE,
+               scale. = TRUE)
+  y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
+  y_train <- as.data.frame(y_train) 
+  sparsevar <- VARshrink(y_train, p=1, type = "none", method = "ridge")
+}
