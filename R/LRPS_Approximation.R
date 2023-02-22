6a0f1f8fc989a51d74691d40382ac6544eced8e8
xinlet <72056199+xinlet@users.noreply.github.com>
2023-02-22 19:58:16 +0000

    Update LRPS_Approximation.R

--- R/LRPS_Approximation.R
+++ R/LRPS_Approximation.R
@@ -11,6 +11,25 @@ library(VARshrink)
 # that K number of factors can explain certain percentage of total variance.
 #======================================
 # Low-Rank Pre-smoothed Data
+LRPS_datagenerate_synthetic <- function(y_train) {
+  pc <- prcomp(y_train,
+               center = TRUE,
+               scale. = TRUE)
+  y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
+  y_train <- as.data.frame(y_train) 
+}
+# LRPS Approximation
+LRPS_Approx_synthetic <- function(y_train) {
+  pc <- prcomp(y_train,
+               center = TRUE,
+               scale. = TRUE)
+  y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
+  y_train <- as.data.frame(y_train) 
+  sparsevar <- VARshrink(y_train, p=1, type = "none", method = "ridge")
+}
+
+
+# Low-Rank Pre-smoothed Data
 LRPS_datagenerate_atlanta <- function(y_train, K) {
   pc <- prcomp(y_train,
                center = TRUE,
@@ -18,7 +37,6 @@ LRPS_datagenerate_atlanta <- function(y_train, K) {
   y_train <- t(t(pc$x[,1:K] %*% t(pc$rotation[,1:K])) * pc$scale + pc$center)
   y_train <- as.data.frame(y_train) 
 }
-
 # LRPS Approximation
 LRPS_Approx_atlanta <- function(y_train, K) {
   pc <- prcomp(y_train,
